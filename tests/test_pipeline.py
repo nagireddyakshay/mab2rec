4eb5d468650f2b72a7c0ebdfad5e5bfba56d0d91
Nagireddy,Akshay <akshayreddy.nagireddy@fmr.com>
2023-06-20 14:40:52 -0400

    Cron test

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -370,9 +370,8 @@ class BenchmarkTest(BaseTest):
         recommenders = deepcopy(self.recommenders)
         recommendations, rec_metrics = benchmark(recommenders, self.metrics, train_data, cv=3,
                                                  user_features=user_features_df)
-        self.assertEqual(len(rec_metrics), 3)
-        print(rec_metrics[0]["Random"]["AUC(score)@3"])
-        self.assertAlmostEqual(rec_metrics[0]["Random"]["AUC(score)@3"], 0.45764725852996097)
+        self.assertEqual(len(rec_metrics), 3)zz
+        self.assertAlmostEqual(rec_metrics[0]["Random"]["AUC(score)@3"], 0.5679790026246719)
         self.assertAlmostEqual(rec_metrics[0]["Random"]["CTR(score)@3"], 0.2616279069767442)
         self.assertAlmostEqual(rec_metrics[0]["Random"]["Precision@3"], 0.05208333333333333)
         self.assertAlmostEqual(rec_metrics[0]["Random"]["Recall@3"], 0.016161228957922595)
